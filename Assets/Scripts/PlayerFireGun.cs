d320fad1086cf7ea8bcb7b8e5c7439692d929e22
Seth Marrison <sethers932@gmail.com>
2022-10-04 17:06:55 +0200

    gun with bullet animation completed
    known issues:
    bullet does not destroy itself when hitting anything

--- Assets/Scripts/PlayerFireGun.cs
+++ Assets/Scripts/PlayerFireGun.cs
@@ -6,15 +6,25 @@ using UnityEngine;
 
 public class PlayerFireGun : MonoBehaviour
 {
+    [Header("Gun Parameters")]
     [SerializeField] private float nextTimeToFire;
     public float fireRate;
     public float range;
-
+    public float damage;
+    
+    [Header("Reload Parameters")]
     public int maxAmmo;
     [SerializeField] private int currentAmmo;
     public float reloadTime;
     [SerializeField] private bool isReloading;
+    
+    [Header("Object Parameters")]
+    public GameObject weaponHolder;
+    public TrailRenderer bulletTrail;
 
+    private Vector3 destination;
+    private float trailSpeed = 250f;
+    
     private void Start()
     {
         currentAmmo = maxAmmo;
@@ -32,7 +42,8 @@ public class PlayerFireGun : MonoBehaviour
             StartCoroutine(Reload());
             return;
         }
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire && currentAmmo > 0)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
@@ -53,14 +64,37 @@ public class PlayerFireGun : MonoBehaviour
     }
     void Shoot()
     {
-        if (currentAmmo > 0)
-        { 
-            currentAmmo  --;
+        currentAmmo  --;
+        Ray ray = new Ray(weaponHolder.transform.position, weaponHolder.transform.forward);
+        RaycastHit hit;
+        destination = ray.GetPoint(range);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            destination = hit.point;
+            Debug.DrawLine(weaponHolder.transform.position, destination, Color.green, 1f);
         }
-       // RaycastHit hit;
-     //   if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, range))
-       // {
-            
-      //  }
+        else
+        {
+            destination = ray.GetPoint(range);
+            Debug.DrawLine(weaponHolder.transform.position, ray.GetPoint(range), Color.green, 1f);
+        }
+
+        InstantiateProjectile(weaponHolder.transform);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+    
+    void InstantiateProjectile(Transform firePoint)
+    {
+        var projectileObj = Instantiate (bulletTrail, firePoint.position, Quaternion.identity);
+        projectileObj.GetComponent<Rigidbody>().velocity = (destination - firePoint.position).normalized * trailSpeed;
     }
 }
